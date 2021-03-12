76e679b4716271c93c4d60b66fdcaec47b45f476
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2021-03-11 21:00:59 -0500

    Redirect to original requested url after login

--- src/component/auth/Login.tsx
+++ src/component/auth/Login.tsx
@@ -4,11 +4,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 
 import { getAuth, useUserContext } from '../../context/AuthProvider';
 
 export default function Login() {
+    const history = useHistory();
     const auth = getAuth();
     const user = useUserContext();
     // Configure FirebaseUI.
@@ -24,10 +25,19 @@ export default function Login() {
             signInSuccessWithAuthResult: () => false
         }
     };
-    if (user) {
-        return <Redirect to="/" />
+
+    // Redirect to the original requested url
+    let initialPath: any = undefined;
+    if (history && history.location && history.location.state) {
+        const state: any = history.location.state;
+        if (state && state.from && state.from.pathname) {
+            initialPath = state.from.pathname;
+        }
+    }
+    if (user && initialPath) {
+        return <Redirect to={initialPath}></Redirect>;
     } else {
-        return <div>
+        return <div id="firebaseui-auth-container">
             <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
         </div>
     }
